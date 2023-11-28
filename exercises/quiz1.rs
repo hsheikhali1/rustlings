d8af83ec76dbfe70acf74ecb858d1c02f937e58b
Harun Sheikhali <harun.sheikhali@agilebits.com>
2023-11-28 13:32:06 -0500

    functions, primitives, and if statements

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(order: u32) -> u32 {
+    (order <= 40).then(|| order * 2).unwrap_or_else(|| order)
+}
+
 
 // Don't modify this function!
 #[test]
